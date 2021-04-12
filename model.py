908405d44663fa8bd392ccbb8c1fb113d84023b5
amulski <amulski@umich.edu>
2021-04-12 15:33:33 -0400

    likelihood functions implemented in model.py

---
+++ model.py
@@ -0,0 +1,23 @@
+# model.py
+# contains functions to calculate the likelihood based on a linear and quadratic model given data and parameters
+
+def calc_linear_likelihood(data, m, b):
+    # model: temp = slope*depth + intercept
+    
+    # prepare data
+    depth = data['Depth'].values
+    temp = data['Temperature'].values
+    temp_error = data['temp_errors'].values
+    likelihood =  np.prod(1. / np.sqrt(2 * np.pi * temp_error ** 2) * np.exp(-(temp - m * depth - b)**2 / (2 * temp_error ** 2) ) )
+    return likelihood
+
+
+def calc_quad_likelihood(data, q, m, b):
+    # model: temp = q*depth^2 + m*depth + b
+    
+    # prepare data
+    depth = data['Depth'].values
+    temp = data['Temperature'].values
+    temp_error = data['temp_errors'].values
+    likelihood =  np.prod(1. / np.sqrt(2 * np.pi * temp_error ** 2) * np.exp(-(temp - q*depth**2 - m * depth - b)**2 / (2 * temp_error ** 2) ) )
+    return likelihood
