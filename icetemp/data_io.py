93a8d2a1e15929119de3024a300b715b02a7484b
joshv2017 <joshv2017@gmail.com>
2021-04-13 14:12:16 -0400

    fixed init file

--- icetemp/data_io.py
+++ icetemp/data_io.py
@@ -17,6 +17,10 @@ def load_ice_data(filename, data_year, temp_errors, depth_errors, data_dir='sout
     data_dir = os.path.join(start_dir, data_dir)
     data_path = os.path.join(start_dir, data_dir, filename)
 
+	print('start', start_dir)
+	print('data', data_dir)
+	print('data path', data_path)
+
     # create pandas DataFrame object from our data
     data = pd.read_csv(data_path, header=None, sep=' |\t', names = ["Temperature", "Depth"], engine='python')
     # specifying the engine prevents a warning message, since we are using regex separators
