bf208f33b8f584bd7849566c76be1160dcc85745
amulski <amulski@umich.edu>
2021-04-20 12:29:44 -0400

    Added simple function for fitting to quadratic function

--- icetemp/model.py
+++ icetemp/model.py
@@ -54,3 +54,61 @@ def calc_quad_likelihood(data, q, m, b):
     temp_error = data['temp_errors'].values
     likelihood =  np.prod(1. / np.sqrt(2 * np.pi * temp_error ** 2) * np.exp(-(temp - q*depth**2 - m * depth - b)**2 / (2 * temp_error ** 2) ) )
     return likelihood
+
+
+def fit_quad(data):
+    """
+    Fits the data to a quadratic function
+    Only errors on temperature are considered in this model
+    Based on Hogg, Bovy, and Lang section 1 (https://arxiv.org/abs/1008.4686)
+    model: temp = q*depth^2 + m*depth + b
+
+    Parameters
+    ----------
+    data : pandas DataFrame
+        data and metadata contained in pandas DataFrame
+        Format described in tutotial notebook
+
+    Returns
+    -------
+    q, m, b (parameters, 1D array of length 3), covariance matrix (3 by 3) : floats, matrix of floats
+        parameters and covariance matrix from fit to quadratic function
+
+    """
+
+    # prepare data
+    depth = data['Depth'].values
+    temp = data['Temperature'].values
+    sigma_y = data['temp_error'].values
+
+    # define quantities from HBL equation 2, 3, and 4
+    Y = temp
+    A = depth[:, np.newaxis] ** (0, 1)
+    C = np.diag(sigma_y ** 2)
+
+
+    C_inv = np.linalg.inv(C)
+    cov = np.linalg.inv(A.T @ C_inv @ A)
+    params = cov @ (A.T @ C_inv @ Y)
+    return params, cov
+
+
+def fit_quad_temp_uncert(data):
+    """
+    Fits the data to a quadratic function using pymc3
+    Errors on temperature are considered in the model
+    model: temp = q*depth^2 + m*depth + b
+
+    Parameters
+    ----------
+    data : pandas DataFrame
+        data and metadata contained in pandas DataFrame
+        Format described in tutotial notebook
+
+    Returns
+    -------
+    q, m, b : floats
+        parameter values from quadratic models
+
+    """
+    return 0
